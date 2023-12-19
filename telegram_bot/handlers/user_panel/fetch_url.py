8b00c2a029722909d970e9e93542088b2477c4d9
yulyakonnova <yulyakonnovaof@gmail.com>
2023-12-19 14:02:50 +0300

    Добавила отправку админу заявки

--- telegram_bot/handlers/user_panel/fetch_url.py
+++ telegram_bot/handlers/user_panel/fetch_url.py
@@ -5,11 +5,17 @@ from aiogram.types import (
 
 )
 from aiogram import F
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import CallbackQuery
+from aiogram.enums import ParseMode
+
 
 from telegram_bot.states import MainMenuStates
-from telegram_bot.service.chat_backends import fetch_url
+from telegram_bot.service.chat_backends import fetch_url, create_keyboard_buttons
 
 from telegram_bot.handlers.user_panel.main_menu import main_menu
+from telegram_bot.assets.config import ADMIN_ID
 
 from bs4 import BeautifulSoup
 import re
@@ -18,13 +24,20 @@ from datetime import datetime
 
 @dp.message(MainMenuStates.user_main_menu, F.text == 'Узнать стоимость')
 async def load_url(message: Message, state: FSMContext):
-    await message.answer("Пожалуйста, укажите URL.")
+    markup = create_keyboard_buttons("Назад")
+    await message.answer("Пожалуйста, пришлите ссылку на интересующую вас модель",
+                         reply_markup=markup)
     await state.set_state(MainMenuStates.report_choose)
 
 
+@dp.message(MainMenuStates.report_choose, F.text == 'Назад')
+async def back_from_load_url(message: Message, state: FSMContext):
+    await main_menu(message, state)
+
 def check_entered_url(url: str):
     if not url.startswith('http') or not url.startswith('https'):
-        return 'Кажется, вы прислали не ссылку! Пожалуйста, пришлите ссылку, которая начинается на http или https'
+        return ('Кажется, вы прислали не ссылку! Пожалуйста, пришлите ссылку, '
+                'которая начинается на http или https')
 
     return 0
 
@@ -51,6 +64,10 @@ async def try_2(message: Message, state: FSMContext):
     if is_valid != 0:
         await message.answer(is_valid)
         return
+
+    await message.answer("Скоро вернемся! Расчет стоимости может занять до 10 минут")
+
+    await state.update_data(url=url)
 
     response = await fetch_url(url)
     await message.reply(f"Размер HTML: {len(response)} символов.")
@@ -97,4 +114,72 @@ async def try_2(message: Message, state: FSMContext):
             if price_match:
                 price = price_match.group(1)
                 print(f"Цена: {price} €")
-        await message.answer(f'Название: {car_name}\n\nСтоимость: {price}€\n\n{characteristcs}')
+
+        builder = InlineKeyboardBuilder()
+        builder.button(text='Отправить заявку', callback_data=f'send_request')
+        markup = builder.as_markup()
+
+        await state.update_data(car_name=car_name, price=price)
+
+        await message.answer(
+            f'Название: {car_name}\n\nСтоимость: {price}€\n\n{characteristcs}',
+            reply_markup=markup
+        )
+
+
+@dp.callback_query(lambda call: call.data == 'send_request', MainMenuStates.report_choose)
+async def send_request_handler(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(MainMenuStates.enter_name_for_request)
+    await callback.message.answer("Пожалуйста, введите ваше имя")
+
+@dp.message(MainMenuStates.enter_name_for_request)
+async def process_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(MainMenuStates.enter_number_for_request)
+    markup = create_keyboard_buttons("Назад")
+    await message.answer(f"Спасибо, {message.text}! Теперь введите ваш контактный номер телефона",
+                         reply_markup=markup)
+
+
+def check_phone_number(telephone_number):
+    input_string = re.sub(r'\D', '', telephone_number)
+    if len(input_string) == 11:
+        return True
+    else:
+        return False
+
+
+@dp.message(MainMenuStates.enter_number_for_request)
+async def process_phone(message: Message, state: FSMContext):
+
+    user_id = message.from_user.id
+    await state.update_data(user_id=user_id)
+
+    telephone_number = message.text
+
+    if telephone_number == "Назад":
+        await load_url(message, state)
+
+    else:
+        if not check_phone_number(telephone_number):
+            await message.answer("Кажется, вы отправили не номер телефона! "
+                                 "Проверьте, пожалуйста, количество цифр")
+            return
+
+        else:
+            telephone_number = re.sub(r'\D', '', telephone_number)
+            await state.update_data(telephone_number=telephone_number)
+            data = await state.get_data()
+
+            user_profile_link = f"<a href='tg://user?id={user_id}'>{data['name']}</a>"
+
+            admin_id = ADMIN_ID
+            await bot.send_message(admin_id, f"Новая заявка от пользователя <b>{user_profile_link}</b>!\n\n"
+                                             f"<b>Контактный номер телефона:</b> {data['telephone_number']}\n\n"
+                                             f"<b>Модель:</b> {data['car_name']}\n\n"
+                                             f"<b>Рассчитанная стоимость:</b> {data['price']} €\n\n"
+                                             f"<a href='{data['url']}'>Ссылка</a>\n",
+                                   parse_mode=ParseMode.HTML)
+
+            await message.answer("Спасибо за заявку! Мы свяжемся с вами в ближайшее время")
+            await main_menu(message, state)
--- telegram_bot/states.py
+++ telegram_bot/states.py
@@ -5,3 +5,5 @@ from aiogram.fsm.context import FSMContext
 class MainMenuStates(StatesGroup):
     user_main_menu = State()
     report_choose = State()
+    enter_name_for_request = State()
+    enter_number_for_request = State()
