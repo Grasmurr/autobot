1355c6061de298c20fdaf3dd12e6d8f4d1dfe277
Суханицкий Роман <suhanickiy.roman@wb.ru>
2023-12-23 21:00:53 +0300

    done parser for both ru and de

--- telegram_bot/handlers/user_panel/fetch_url.py
+++ telegram_bot/handlers/user_panel/fetch_url.py
@@ -12,7 +12,7 @@ from telegram_bot.service.chat_backends import fetch_url
 from telegram_bot.handlers.user_panel.main_menu import main_menu
 
 from bs4 import BeautifulSoup
-import re
+import re, json
 from datetime import datetime
 
 
@@ -43,6 +43,7 @@ def check_car_age(registration_date):
     else:
         return True
 
+
 @dp.message(MainMenuStates.report_choose)
 async def try_2(message: Message, state: FSMContext):
     url = message.text
@@ -59,42 +60,84 @@ async def try_2(message: Message, state: FSMContext):
 
     car_data = {}
 
-    attribute_blocks = soup.find_all("div", class_="g-row")
+    lang = soup.html.get('lang')
 
-    for block in attribute_blocks:
-        spans = block.find_all('span')
-        if len(spans) >= 2:
-            key = spans[0].get_text(strip=True)
-            value = spans[1].get_text(strip=True)
-            car_data[key] = value
+    if lang == 'ru':
+        car_name = soup.title.string
 
-        p_tags = block.find_all('p', class_='bullet-point-text')
-        for p in p_tags:
-            feature = p.get_text(strip=True)
-            car_data[feature] = "Да"
+        attribute_blocks = soup.find_all("div", class_="g-row")
 
-    print (car_data['Первая регистрация'])
+        for block in attribute_blocks:
+            spans = block.find_all('span')
+            if len(spans) >= 2:
+                key = spans[0].get_text(strip=True)
+                value = spans[1].get_text(strip=True)
+                car_data[key] = value
 
-    registration_date = car_data.get('Первая регистрация', '')
-    if not check_car_age(registration_date):
-        await message.answer('Машина старше пяти лет! На машины старше пяти лет высокая пошлина '
-                             'и их ввозить не выгодно.')
-        await main_menu(message, state)
-    else:
+            p_tags = block.find_all('p', class_='bullet-point-text')
+            for p in p_tags:
+                feature = p.get_text(strip=True)
+                car_data[feature] = "Да"
+
+        # print (car_data['Первая регистрация'])
+        # registration_date = car_data.get('Первая регистрация', '')
+        # if not check_car_age(registration_date):
+        #     await message.answer('Машина старше пяти лет! На машины старше пяти лет высокая пошлина '
+        #                          'и их ввозить не выгодно.')
+        #     await main_menu(message, state)
+        # else:
         characteristcs = ''
         keys = [i for i in car_data]
         values = [i for i in car_data.values()]
         for i in range(len(keys)):
             characteristcs += f'{keys[i]} {values[i]}\n'
-        car_name = soup.title.string.split(' в городе ')[0]
         await message.answer(f'{car_name}')
 
-        price_text = soup.find(text=re.compile("Брутто"))
-        print (characteristcs)
+        print(characteristcs)
 
-        if price_text:
-            price_match = re.search(r'(\d+\s*\d*\s*\d*)\s*€\s*\(*Брутто\)*', price_text)
-            if price_match:
-                price = price_match.group(1)
-                print(f"Цена: {price} €")
-        await message.answer(f'Название: {car_name}\n\nСтоимость: {price}€\n\n{characteristcs}')
+        netto_price = soup.find(class_="netto-price").text
+
+        await message.answer(f'Название: {car_name}\n\nСтоимость: {netto_price}€\n\n{characteristcs}')
+    else:
+        car_name = soup.title.string
+        scripts = soup.find_all("script")
+        kilometerstand = None
+        for script in scripts:
+            if script.string and '"mileage":' in script.string:
+                match = re.search(r'"mileage":\s*"([^"]+)"', script.string)
+                if match:
+                    kilometerstand = match.group(1)
+                    break
+
+        netto_price_element = soup.find(attrs={"data-testid": "sec-price"})
+        if netto_price_element:
+            netto_price = netto_price_element.text
+            print("Netto Price found with data-testid:", netto_price)
+        else:
+            scripts = soup.find_all("script")
+            netto_price = None
+            for script in scripts:
+                if script.string and '"price":' in script.string:
+                    match = re.search(r'"price":\s*([0-9.]+)', script.string)
+                    if match:
+                        netto_price = match.group(1)
+                        break
+
+        kilometerstand = soup.find('div', id='mileage-v').get_text(strip=True)
+        erstzulassung = soup.find('div', id='firstRegistration-v').get_text(strip=True)
+        leistung = soup.find('div', id='power-v').get_text(strip=True)
+        getriebe = soup.find('div', id='transmission-v').get_text(strip=True)
+        fahrzeughalter = soup.find('div', id='numberOfPreviousOwners-v').get_text(strip=True)
+        kraftstoffart = soup.find('div', id='fuel-v').get_text(strip=True)
+
+        await message.answer(
+            f'Название: {car_name}\n\n'
+            f'Стоимость нетто: {netto_price}€\n'
+            f'Пробег: {kilometerstand}\n'
+            f'Первая регистрация: {erstzulassung}\n'
+            f'Мощность: {leistung}\n'
+            f'Тип коробки передач: {getriebe}\n'
+            f'Количество владельцев: {fahrzeughalter}\n'
+            f'Тип топлива: {kraftstoffart}'
+        )
+
